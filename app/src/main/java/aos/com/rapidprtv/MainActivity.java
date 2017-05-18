b1ed32b6d2ab71864a6e322a8a3321c7df410381
ar13101085 <ar13101085@gmail.com>
2017-05-18 20:47:26 +0600

    Signed-off-by: ar13101085 <ar13101085@gmail.com>

--- app/src/main/java/aos/com/rapidprtv/MainActivity.java
+++ app/src/main/java/aos/com/rapidprtv/MainActivity.java
@@ -1,13 +1,27 @@
 package aos.com.rapidprtv;
 
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView TvVideoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_design);
+        TvVideoView= (VideoView) findViewById(R.id.TvVideoView);
+
+        /*MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(TvVideoView);
+        TvVideoView.setMediaController(mediaController);*/
+        TvVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test_video));
+        TvVideoView.start();
+
+
     }
 }
